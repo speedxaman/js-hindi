f563b7fb41c9391484261e599ba226103ca2b2ae
Aman Gautam <116288913+speedxaman@users.noreply.github.com>
2024-03-06 19:10:17 +0000

    learned datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+// pehle ki js me classes,modules, ye sb nh tha to or ab h to purana code disturb na ho is liye "use strict" use krte h
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "aman" // string
+let age = 18 // number
+let isLoggedIn = false; // boolean
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => we have not defined value
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
