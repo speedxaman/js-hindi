7b2146ae1c28fa68da980d80e420de2ab1925d26
Aman Gautam <116288913+speedxaman@users.noreply.github.com>
2024-03-11 19:05:00 +0000

    comparisons

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,4 +25,15 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
-// 01:14:46 -  Why string to number
+// ******************* OPERATIONS *******************
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+////// ***************IMpotamt******************
+// The reason is that == and ><>=<= works differently
+// comparison converts null to a number treating it as 0
+// that why null >= 0 is true and null > 0 is false
+console.log("02">1,"line");
+
+
+
