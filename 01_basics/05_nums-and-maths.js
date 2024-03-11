161bc74e1f5c9dbad6522376b3dc08ecfe22af4a
Aman Gautam <116288913+speedxaman@users.noreply.github.com>
2024-03-11 20:16:16 +0000

    more things about floor,ceil,round

--- 01_basics/05_nums-and-maths.js
+++ 01_basics/05_nums-and-maths.js
@@ -19,8 +19,8 @@ const hundreds = 1000000
 // console.log(Math);
 // console.log(Math.abs(-4));
 // console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
+// console.log(Math.ceil(4.2)); // ceiling matlab top value
+// console.log(Math.floor(4.9)); // floor matlab niceh wali value
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
@@ -32,3 +32,6 @@ const min = 10
 const max = 20
 
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+///// 02:52:34  -  date and time
