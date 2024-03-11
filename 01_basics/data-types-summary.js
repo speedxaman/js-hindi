63bf46627b08128ba13fbd5a1b7f8f97430a2de8
Aman Gautam <116288913+speedxaman@users.noreply.github.com>
2024-03-11 19:36:44 +0000

    stack and heap learned

--- 01_basics/data-types-summary.js
+++ 01_basics/data-types-summary.js
@@ -36,3 +36,29 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//// STACK(primitive) , HEAP(Non-primitive)
+
+let myName = "Amankg"
+let anotherName = myName;
+
+anotherName = "gautam";
+
+// console.log(myName);
+// console.log(anotherName);
+
+let userOne = {
+    name:"aman",
+    age:22
+}
+
+let userTwo = userOne;
+userTwo.name = "gautam";
+
+// console.log(userTwo.name);
+// console.log(userOne.name);
+
+// heap me hame refernce of original object milta h to agar koi change krege jo wo original value me bhi change ho jayega
+// stack me hame copy milti h to original value change nh hoti
+
